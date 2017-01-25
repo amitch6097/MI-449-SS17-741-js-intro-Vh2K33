d6937836fcbcb71819233acd7f4a66586bb9548b
Andrew Mitchell <mitch702@msu.edu>
2017-01-24 23:49:08 -0500

    adding project

---
+++ index.js
@@ -0,0 +1,47 @@
+//buttons
+var openhatButton = document.getElementById('openhat-button')
+var hihatButton = document.getElementById('hihat-button')
+var boomButton = document.getElementById('boom-button')
+var kickButton = document.getElementById('kick-button')
+var snareButton = document.getElementById('snare-button')
+//sounds
+var hihatSound = document.getElementById('hihat')
+var boomSound = document.getElementById('boom')
+var openhatSound = document.getElementById('openhat')
+var kickSound = document.getElementById('kick')
+var snareSound = document.getElementById('snare')
+//boom listners
+boomButton.addEventListener('click', function(){
+  boomSound.play()
+})
+boomButton.addEventListener('mouseenter', function(){
+  boomSound.play()
+})
+//hihat listners
+hihatButton.addEventListener('click', function(){
+  hihatSound.play()
+})
+hihatButton.addEventListener('mouseenter', function(){
+  hihatSound.play()
+})
+//openhat listners
+openhatButton.addEventListener('click', function(){
+  openhatSound.play()
+})
+openhatButton.addEventListener('mouseenter', function(){
+  openhatSound.play()
+})
+//kick listners
+kickButton.addEventListener('click', function(){
+  kickSound.play()
+})
+kickButton.addEventListener('mouseenter', function(){
+  kickSound.play()
+})
+//openhat listners
+snareButton.addEventListener('click', function(){
+  snareSound.play()
+})
+snareButton.addEventListener('mouseenter', function(){
+  snareSound.play()
+})
