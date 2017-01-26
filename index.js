c71ac9a3931711a3b627bdc8e43aa332a6f8521e
Andrew Mitchell <mitch702@msu.edu>
2017-01-26 14:44:38 -0500

    compressing code

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
-//buttons
-var openhatButton = document.getElementById('openhat-button')
-var hihatButton = document.getElementById('hihat-button')
-var boomButton = document.getElementById('boom-button')
-var kickButton = document.getElementById('kick-button')
-var snareButton = document.getElementById('snare-button')
-//sounds
-var hihatSound = document.getElementById('hihat')
-var boomSound = document.getElementById('boom')
-var openhatSound = document.getElementById('openhat')
-var kickSound = document.getElementById('kick')
-var snareSound = document.getElementById('snare')
-//boom listners
-boomButton.addEventListener('click', function(){
-  boomSound.play()
-})
-boomButton.addEventListener('mouseenter', function(){
-  boomSound.play()
-})
-//hihat listners
-hihatButton.addEventListener('click', function(){
-  hihatSound.play()
-})
-hihatButton.addEventListener('mouseenter', function(){
-  hihatSound.play()
-})
-//openhat listners
-openhatButton.addEventListener('click', function(){
-  openhatSound.play()
-})
-openhatButton.addEventListener('mouseenter', function(){
-  openhatSound.play()
-})
-//kick listners
-kickButton.addEventListener('click', function(){
-  kickSound.play()
-})
-kickButton.addEventListener('mouseenter', function(){
-  kickSound.play()
-})
-//snare listners
-snareButton.addEventListener('click', function(){
-  snareSound.play()
-})
-snareButton.addEventListener('mouseenter', function(){
-  snareSound.play()
-})
+var addSoundToButton = function (soundName) {
+  //get button
+  var button = document.getElementById(soundName + '-button')
+  //get sound
+  var sound = document.getElementById(soundName)
+  //add listners
+  button.addEventListener('click', function(){
+    sound.play()
+  })
+  button.addEventListener('mouseenter', function(){
+    sound.play()
+  })
+}
+//add events listners for every button
+addSoundToButton('openhat')
+addSoundToButton('hihat')
+addSoundToButton('boom')
+addSoundToButton('kick')
+addSoundToButton('snare')
